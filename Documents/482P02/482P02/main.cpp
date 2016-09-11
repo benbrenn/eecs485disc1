98ab2f3cc52da72ac0c1cf9c5c40dbe07a325e4d
Ben Brenner <benbrenner@Bens-MacBook-Pro.local>
2016-09-11 19:36:41 -0400

    gitignore

---
+++ Documents/482P02/482P02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  482P02
+//
+//  Created by Ben Brenner on 2/2/16.
+//  Copyright (c) 2016 Ben Brenner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
